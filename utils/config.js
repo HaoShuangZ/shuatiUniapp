09ac8021ed9d8f387bab8a67db0f9ebbbf950060
zhs <sygence@gmail.com>
2024-01-14 17:49:18 +0800

    fix:debug

--- utils/config.js
+++ utils/config.js
@@ -2,10 +2,10 @@
 // const baseUrl = 'http://39.98.88.24:8080/customer/api/project' //请求的基本路径
 // const proUrl = 'https://118.31.52.116:8088/customer/api/project' //生产环境地址环境地址
 // const baseUrl = 'https://mail.sygence.com:8080/customer/api/project' //生产环境地址环境地址
-const baseUrl = 'https://api.shuatitong.com:8088/customer/api/project' // 开发环境地址
-const proUrl = 'https://api.shuatitong.com:8088/customer/api/project' // 开发环境地址
+const baseUrl = '' // 开发环境地址
+const proUrl = '' // 开发环境地址
 const modalColor = '#5271FF' //弹窗颜色 
-const appId = "wxefcc249223e2f851"
+const appId = ""
 
 
 export {
